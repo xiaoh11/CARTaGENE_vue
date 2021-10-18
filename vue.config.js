5fdc9519a401d482e816607848bd230f103ec741
Colin Gross <grosscol@gmail.com>
2021-10-18 13:07:53 -0400

    Simplify apache config requirements.   Emit html files without extension to simplify rewrite rules.

--- vue.config.js
+++ vue.config.js
@@ -18,42 +18,42 @@ module.exports = {
     variant: {
       entry: 'src/pages/variant/main.js',
       template: '/src/pages/variant/variant.html',
-      filename: 'variant.html',
+      filename: 'variant',
       title: 'Variant Page',
       chunks: ['chunk-vendors', 'chunk-common', 'variant']
     },
     region: {
       entry: 'src/pages/region/main.js',
       template: '/src/pages/region/region.html',
-      filename: 'region.html',
+      filename: 'region',
       title: 'Region Page',
       chunks: ['chunk-vendors', 'chunk-common', 'region']
     },
     gene: {
       entry: 'src/pages/gene/main.js',
       template: '/src/pages/gene/gene.html',
-      filename: 'gene.html',
+      filename: 'gene',
       title: 'Gene Page',
       chunks: ['chunk-vendors', 'chunk-common', 'gene']
     },
     terms: {
       entry: 'src/pages/terms/main.js',
       template: '/src/pages/terms/terms.html',
-      filename: 'terms.html',
+      filename: 'terms',
       title: 'Data Use Terms',
       chunks: ['chunk-vendors', 'chunk-common', 'terms']
     },
     about: {
       entry: 'src/pages/about/main.js',
       template: '/src/pages/about/terms.html',
-      filename: 'about.html',
+      filename: 'about',
       title: 'About Bravo',
       chunks: ['chunk-vendors', 'chunk-common', 'about']
     },
     404: {
       entry: 'src/pages/about/main.js',
       template: '/src/pages/about/terms.html',
-      filename: '404.html',
+      filename: '404',
       title: 'About Bravo',
       chunks: ['chunk-vendors', 'chunk-common', '404']
     },
