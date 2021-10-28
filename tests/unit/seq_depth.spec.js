8a2bb0f5e879bed13d506a31547a3914c947ae67
Colin Gross <grosscol@gmail.com>
2021-10-28 13:04:45 -0400

    Fix naming typo

--- tests/unit/seq_depth.spec.js
+++ tests/unit/seq_depth.spec.js
@@ -1,11 +1,11 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import RegionDashboard from '@/components/SeqDepth.vue'
+import SeqDepth from '@/components/SeqDepth.vue'
 
 // Need to mock data loading before implementing tests on SeqDepth
 describe.skip('SeqDepth structure.', () => {
   it('renders a svg node', () => {
-    const wrapper = shallowMount(RegionDashboard)
+    const wrapper = shallowMount(SeqDepth)
     const svg = wrapper.find('svg')
     expect(svg.exists()).to.be.true
   })
