25f70a811bf5f0ffdf086b584a12221bc532a7e3
Colin Gross <grosscol@gmail.com>
2021-11-05 16:05:54 -0400

    Implement base boolean filter button.   Provide initial content for subsequent class encapsulation.

--- src/components/FilterBar.vue
+++ src/components/FilterBar.vue
@@ -3,7 +3,8 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-12" style="padding-left: 0px; padding-top: 2px; padding-bottom: 2px;">
-          <QualityFilterButton v-bind="$attrs"/>
+          <QualityFilterButton v-bind="$attrs" givenCategory="quality"/>
+          <BaseFilterButton v-bind="$attrs"/>
           <!--
           <AnnotationFilterButton/>
           <LOFTEEFilterButton/>
@@ -19,6 +20,7 @@
 
 <script>
 import QualityFilterButton from '@/components/filters/QualityFilterButton.vue'
+import BaseFilterButton from '@/components/filters/BaseFilterButton.vue'
 export default {
   name: "FilterBar",
   inheritAttrs: false,
@@ -29,7 +31,8 @@ export default {
     }
   },
   components: {
-    QualityFilterButton
+    QualityFilterButton,
+    BaseFilterButton
   },
 }
 </script>
---
+++ src/components/filters/BaseFilterButton.vue
@@ -0,0 +1,262 @@
+<template>
+    <div class="parent-menu" v-click-outside="handleClickAway">
+      <div style="display: inline-block;">
+        <button class="btn btn-sm dropdown-toggle" :class="buttonClass" v-on:click="showDropDown = !showDropDown">
+          {{compositeTitle}} 
+        </button>
+        <div v-if="showDropDown" class="dropdown-menu shadow show">
+          <form class="p-2">
+
+            <template v-for="(group, groupKey) in eFilter" :key="groupKey">
+              <li>
+                <div class="custom-control custom-checkbox">
+                  <input class="custom-control-input" type="checkbox" :value="groupKey" 
+                    :id="groupKey" v-model="group.allTrue" @change="handleGroupToggle(group)">
+                  <label class="custom-control-label" :for="groupKey">{{group.title}}</label>
+                </div>
+                <ul style="list-style-type: none; padding-left: 1rem; padding-top: 0.2rem;">
+                <template v-for="(member, memberKey) in group.members" :key="memberKey">
+                  <li>
+                    <div class="custom-control custom-checkbox">
+                      <input class="custom-control-input" type="checkbox" :value="memberKey" :id="memberKey" 
+                             v-model="member.val" @change="handleMemberToggle(member, group)">
+                      <label class="custom-control-label" :for="memberKey">{{member.title}}</label>
+                      <small class="form-text text-muted">{{member.desc}}</small>
+                    </div>
+                  </li>
+                </template>
+                </ul>
+              </li>
+            </template>
+
+            <hr/>
+            <div class="form-row">
+              <div class="col mr-auto">
+                <button type="button" class="btn btn-secondary btn-sm" v-on:click="clearEphemeralFilters">
+                  Clear
+                </button>
+              </div>
+              <div class="col mr-auto">
+                <button type="button" class="btn btn-primary btn-sm float-right" v-on:click="applyFilters">
+                  Apply
+                </button>
+              </div>
+            </div>
+          </form>
+
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  import clone from 'just-clone';
+
+  export default {
+    name: "BaseFilterButton",
+    props: {
+      title: {
+        type: String,
+        default: "Filters"
+      },
+      givenCategory: {
+        type: String,
+        default: "base"
+      }
+    },
+    data: function() {
+      return {
+        // persisted form values. Loaded on show. Updated on save. 
+        pFilter: {
+          gColorFilter: {
+            title: "Example Filter Group",
+            allTrue: false,
+            members: {
+              red: { 
+                title: "Red", 
+                val: false,
+                desc: "Is red."
+              },
+              green: { 
+                title: "Green", 
+                val: false,
+                desc: "Is green"
+              }
+            }
+          },
+          gQCFails:{
+            title: "Quality Control Failures",
+            allTrue: false,
+            members: {
+              mainQC: { 
+                title: "Main Quality Control", 
+                val: false,
+                desc: "Failed primary quality control"
+              },
+              secondary: { 
+                title: "Secondary", 
+                val: false,
+                desc: "Failed Secondary quality control"
+              },
+              alice: { 
+                title: "Project alICE", 
+                val: false,
+                desc: "Failed QC for the fictional alICE project"
+              }
+            }
+          }
+        },
+        // ephemeral filter model the form edits directly
+        eFilter: {},
+        filterVals: [{field: "filter", type: "=", value: "PASS"}],
+        showDropDown: false
+      }
+    },
+    emits: ['filterChange'],
+    created: function() {
+      // initial setup of ephemeral filters to ensure they're never empty.
+      this.eFilter = clone(this.pFilter)
+    },
+    computed: {
+      buttonClass(){ 
+        if(this.appliedFiltersCount > 0){ 
+          return('btn-primary')
+        }
+        else {
+          return('btn-outline-primary')
+        }
+      },
+      compositeTitle(){
+        if( this.appliedFiltersCount > 0 ) {
+          return( this.title + ` (${this.appliedFiltersCount})`)
+        } else {
+          return( this.title )
+        }
+      },
+      dropDownClass(){
+        if(this.showDropDown){
+          return("parent-menu-dropdown shadow")
+        }
+        else {
+          return("parent-menu-dropdown shadow show")
+        }
+      },
+      appliedFiltersCount(){
+        let vals = Object.values(this.pFilter)
+          .map(g => Object.values(g.members))
+          .flat()
+          .map(m => m.val)
+        let sum = vals.reduce((acc, curr) => acc + curr, 0)
+        return(sum)
+      }
+    },
+    watch: {
+      // When showing the drop down, load the persistent filters.
+      showDropDown: function(val){
+        if(val){
+          this.eFilter = clone(this.pFilter)
+        }
+      }
+    },
+    methods: {
+      isEntireGroupTrue: function(filterGroup){
+        let verdict = Object.values(filterGroup.members)
+          .map(m => m.val)
+          .every(v => v)
+        return verdict
+      },
+      filterGroupToArray: function(filterGroup){
+        let arr = []
+        Object.entries(filterGroup.members).forEach( ([key, content]) => {
+          if(content.val === true){
+            arr.push({ field: 'filter', type: '=', value: key})
+          }
+        })
+        return(arr)
+      },
+      filterToArray: function(filter){
+        let arr = Object.values(filter)
+          .map(g => this.filterGroupToArray(g))
+          .flat()
+        return(arr)
+      },
+      emitFilterChange: function (){
+        this.$emit('filterChange', this.givenCategory, this.filterToArray(this.pFilter))
+      },
+      applyFilters: function() {
+        this.showDropDown = false
+
+        // If selection changed, make ephemeral selection persistent
+        if( JSON.stringify(this.pFilter) === JSON.stringify(this.eFilter)){
+          return
+        }
+        this.pFilter = clone(this.eFilter)
+        this.emitFilterChange()
+      },
+      setGroupVals: function(filterGroup, val){
+        for(let member of Object.values(filterGroup.members)){
+          member.val = val
+        }
+      },
+      clearEphemeralFilters: function() {
+        for(let group of Object.values(this.eFilter)){
+          group.allTrue = false
+          this.setGroupVals(group, false)
+        }
+      },
+      handleClickAway: function(){
+        this.showDropDown = false
+      },
+      handleGroupToggle: function(group){
+        // v-model fires after on click, so eventual value is opposite of observed here.
+        // set group to match eventual value
+        this.setGroupVals(group, group.allTrue)
+      },
+      handleMemberToggle: function(member, group){
+        group.allTrue = this.isEntireGroupTrue(group)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .parent-menu {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    display: inline-block;
+    font-size: 14px;
+  }
+  .parent-menu-button {
+    outline: none;
+    padding: 0px 7px 0px 7px;
+    margin: 1px 1px 1px 1px;
+    color: white;
+    background-color: #007bff;
+    border: 1px solid #007bff;
+    border-radius: 2px;
+    box-shadow: none;
+  }
+  .parent-menu-button:hover {
+    background-color: #0062cc;
+    border-color: #0062cc;
+  }
+  .parent-menu-dropdown {
+    display: block;
+    position: absolute;
+    background-color: #eeeeee;
+    min-width: 140px;
+    overflow: auto;
+    box-shadow: 0px 4px 8px 0px rgba(0,0,0,0.2);
+    border: 1px solid #cbcacb;
+    z-index: 999;
+  }
+  .parent-menu-dropdown a {
+    display: block;
+    padding: 4px 2px;
+    color: black;
+    text-align: left;
+    text-decoration: none;
+  }
+  .parent-menu-dropdown a:hover {
+    background-color: #cccccc;
+  }
+</style>
