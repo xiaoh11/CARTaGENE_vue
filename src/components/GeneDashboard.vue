cbc26a8a25be2a61c0d6641ececd3792e00ac8cd
Colin Gross <grosscol@gmail.com>
2021-12-17 13:43:18 -0500

    Rough in gene dashboard strcture Begin DRY-ing tab styling.

---
+++ src/components/GeneDashboard.vue
@@ -0,0 +1,128 @@
+<template>
+  <div id="bravo-plot"> 
+    <p>GeneDashboard</p>
+    <!--
+    <GeneInfo/>
+    -->
+    <div id="bravoviz">
+      <div class="parent-menu">
+        <ToggleList list-title="Panels" list-group="showPanels" :list-vars="showPanels"
+          @varToggled="handleInfoViewToggle" :icon="panelsIcon"/>
+        <ToggleList list-title="Columns" list-group="showCols" :list-vars="showCols"
+          @varToggled="handleInfoViewToggle" :icon="columnsIcon"/>
+
+        <div class="d-none d-sm-inline" style="display: inline-block;"> 
+          <button type="button" class="parentMenu__button" v-on:click="doDownload++">
+            CSV
+            <font-awesome-icon style="background-color: transparent; display: inline-block; vertical-align: middle" :icon="downloadIcon"></font-awesome-icon>
+          </button>
+        </div>
+      </div>
+      <div style="position: relative; min-height: 20px">
+        <!--
+        <geneSummaries v-if="showPanels.summaries.val" :filterArray='filterArray'
+          @close="showPanels.summaries.val = false"/>
+        <SeqDepth v-if="showPanels.seqDepth.val" @close="showPanels.seqDepth.val = false" 
+          :hoveredVariant="hoveredVariant" :segmentBounds="segmentBounds" 
+          :segmentRegions="segmentRegions" :givenWidth="childWidth" :givenMargins="childMargins"/>
+        <GeneBars v-if="showPanels.genes.val" @close="showPanels.genes.val = false" 
+          :hoveredVariant="hoveredVariant" :segmentBounds="segmentBounds" 
+          :segmentRegions="segmentRegions" :givenWidth="childWidth" :givenMargins="childMargins"/>
+        <SnvCount v-if="showPanels.snvCount.val" @close="showPanels.snvCount.val = false" 
+          :segmentBounds="segmentBounds" 
+          :segmentRegions="segmentRegions" :givenWidth="childWidth" :givenMargins="childMargins"
+          :filters="filterArray" :visibleVariants="visibleVariants"/>
+        <BpCoordBar :segmentBounds="segmentBounds" :segmentRegions="segmentRegions" 
+          :givenWidth="childWidth" :givenMargins="childMargins" />
+        <FilterBar @filterChange='handleFilterChange'/>
+        <GeneSNVTable :filters="filterArray" :doDownload="doDownload"/>
+        -->
+      </div>
+      <pre>
+        DEBUG
+        gene: {{gene}}
+        chrom: tbd
+        start: tbd
+        stop: tbd
+        filterArray: {{filterArray}}
+      </pre>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faWindowRestore, faDownload, faColumns } 
+  from '@fortawesome/free-solid-svg-icons'
+import clone from 'just-clone'
+//import RegionInfo      from '@/components/RegionInfo.vue'
+//import RegionSummaries from '@/components/RegionSummaries.vue'
+import FilterBar       from '@/components/FilterBar.vue'
+import ToggleList      from '@/components/ToggleList.vue'
+import SeqDepth        from '@/components/SeqDepth.vue'
+import GeneBars        from '@/components/GeneBars.vue'
+import SnvCount        from '@/components/SnvCount.vue'
+import BpCoordBar      from '@/components/BpCoordBar.vue'
+//import RegionSNVTable  from '@/components/tables/RegionSNVTable.vue'
+
+export default {
+  name: 'GeneDashboard',
+  components: {
+    FontAwesomeIcon,
+    //RegionInfo,
+    //RegionSummaries,
+    FilterBar,
+    ToggleList,
+    SeqDepth,
+    GeneBars,
+    SnvCount,
+    BpCoordBar,
+    //RegionSNVTable
+  },
+  inject: {
+    gene: {default: null}
+  },
+  data: function(){
+    return {
+      panelsIcon: faWindowRestore,
+      columnsIcon: faColumns,
+      downloadIcon: faDownload,
+      doDownload: 0,
+      showPanels: {
+        summaries: {title: "Summary", val: true},
+        seqDepth:  {title: "Avg. Depth", val: true},
+        genes:     {title: "Genes", val: true},
+        snvCount:  {title: "Variants Count", val: true},
+      },
+      showCols: {
+        variantID:      { title: "Variant ID", val: true},
+        rsID:           { title: "rsID", val: true},
+        consequence:    { title: "Consequence", val: true},
+        annotation:     { title: "Annotation", val: true},
+        LOFTEE:         { title: "LOFTEE", val: true},
+        quality:        { title: "Quality", val: true},
+        CADD:           { title: "CADD", val: true},
+        nAlleles:       { title: "N Alleles", val: false},
+        het:            { title: "Het", val: true},
+        homAlt:         { title: "Hom Alt", val: true},
+        frequency:      { title: "Frequency (%)", val: true}
+      },
+      showTableMenuDropDown: false,
+      // keys are category of filter,
+      // values are array of mongo-like filters.
+      filter: {},
+    }
+  },
+  computed: {
+    filterArray: function() {
+      return(Object.values(this.filter).flat(1))
+    },
+  },
+  methods:{
+    handleInfoViewToggle: function(listGroup, varKey){
+      this[listGroup][varKey].val = !this[listGroup][varKey].val
+    },
+  }
+}
+</script>
---
+++ src/pages/gene/GenePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <NavBar style="margin-left: 5px;"/>
+
+  <div class="container-fluid">
+    <div class="row justify-content-center" style="border-bottom: 1px solid #cccccc;margin-bottom: 5px;">
+      <div class="col-12 col-sm-11 col-md-11">
+        <div class="bravo-tab">
+          <div class="bravo-tab-item bravo-tab-item-selected">
+            <a href="#">SNVs and Indels</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-12 col-sm-11 col-md-11">
+        <GeneDashboard/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from '../../components/NavBar.vue'
+import GeneDashboard from '../../components/GeneDashboard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    GeneDashboard
+  },
+  provide: function() {
+    let urlParams = new URLSearchParams(window.location.search)
+    return {
+      gene: urlParams.get('id')
+    }
+  }
+}
+</script>
+
