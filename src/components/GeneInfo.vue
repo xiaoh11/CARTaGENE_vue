24321df8e30c4d7f63912566d104a51b5b6ddfc7
Colin Gross <grosscol@gmail.com>
2021-12-22 17:39:17 -0500

    Do url generation as computed attrs to improve clarity.

--- src/components/GeneInfo.vue
+++ src/components/GeneInfo.vue
@@ -7,19 +7,19 @@
           <h6>{{geneData.full_gene_name}}</h6>
           <ul class="list-unstyled">
             <li class="list-item">
-              Ensembl ID: <a v-bind:href="'http://www.ensembl.org/Homo_sapiens/geneview?gene=' + geneData.gene_id">{{geneData.gene_id}}</a>
+              Ensembl ID: <a v-bind:href="ensemblUrl">{{geneData.gene_id}}</a>
             </li>
             <li class="list-item">
               Gene type: <span style="color:#85144b;">{{geneData.gene_type}}</span>
             </li>
             <li class="list-item">
-              Region: <a href="#">{{chrom}}:{{start.toLocaleString()}}-{{stop.toLocaleString()}}</a></li>
+              Region: <a :href="bravoRegionUrl">{{chrom}}:{{start.toLocaleString()}}-{{stop.toLocaleString()}}</a></li>
             <li class="list-item">Total length: {{regionLength.toLocaleString()}} bp</li>
             <li class="list-item">Exonic length: {{exonicLength.toLocaleString()}} bp</li>
             <li class="list-item">External resources:
-              <span><a v-bind:href="'http://pheweb.sph.umich.edu/SAIGE-UKB/gene/' + geneData.gene_name">UK Biobank PheWeb</a></span>
-              <span>, <a v-bind:href="'https://omim.org/entry/' + geneData.omim_accession">OMIM</a></span>
-              <span>, <a v-bind:href="'https://www.genecards.org/cgi-bin/carddisp.pl?gene=' + geneData.gene_id">GeneCards</a></span>
+              <span><a v-bind:href="phewebUrl">UK Biobank PheWeb</a></span>
+              <span>, <a v-bind:href="omimUrl">OMIM</a></span>
+              <span>, <a v-bind:href="geneCardUrl">GeneCards</a></span>
               <span>, <a v-bind:href="ucscUrl">UCSC Browser</a></span>
             </li>
           </ul>
@@ -65,10 +65,25 @@ export default {
     regionLength() {
       return ((this.stop - this.start + 1))
     },
+    bravoRegionUrl(){
+      return(`/region.html?variant_type=snv&chrom=${this.chrom}&start=${this.start}&stop=${this.stop}`)
+    },
     ucscUrl() {
       return ('https://genome.ucsc.edu/cgi-bin/hgTracks?db=GRCh38&position=' +
         this.chrom + ':' + this.start + '-' + this.stop)
-    }
+    },
+    geneCardUrl() {
+      return('https://www.genecards.org/cgi-bin/carddisp.pl?gene=' + this.geneData.gene_id)
+    },
+    phewebUrl() {
+      return('http://pheweb.sph.umich.edu/SAIGE-UKB/gene/' + this.geneData.gene_name)
+    },
+    omimUrl() {
+      return('https://omim.org/entry/' + this.geneData.omim_accession)
+    },
+    ensemblUrl() {
+      return('http://www.ensembl.org/Homo_sapiens/geneview?gene=' + this.geneData.gene_id)
+    },
   },
 }
 </script>
