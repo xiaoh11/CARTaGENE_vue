b99c4e39a4500463bdaa749b6f0421ae2f2616df
Colin Gross <grosscol@gmail.com>
2022-01-10 11:35:05 -0500

    Remove unused navbar element.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,9 +3,6 @@
       <a class="navbar-brand" v-bind:href="publicPath">
         <img class="bo-small-logo" width="34" v-bind:src="logo"/>
       </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button> 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item"><a class="nav-link btn-sm" href="/about.html">About</a></li>
