5cca69e7358612c32fb47de9a6f1bb29e2f5ab56
Colin Gross <grosscol@gmail.com>
2022-11-01 12:24:02 -0400

    Rely on css assests for styling.

--- src/pages/terms/Terms.vue
+++ src/pages/terms/Terms.vue
@@ -29,14 +29,3 @@ export default {
     
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 0px;
-  margin-left: 5px;
-}
-</style>
