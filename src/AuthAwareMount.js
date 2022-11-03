5175c504b44ec5c0cda35ca6e6940d40abc9f81d
Colin Gross <grosscol@gmail.com>
2022-11-03 11:51:03 -0400

    Remove debugging print to console.

--- src/AuthAwareMount.js
+++ src/AuthAwareMount.js
@@ -20,12 +20,10 @@ function authExpectedMount(app, mountPoint){
       if(resp.data.login_disabled){
         app.mount(mountPoint)
       }else if(resp.data.authenticated){
-        console.log('authenticated')
         app.provide('user', resp.data.user) 
         app.provide('loginDisabled', resp.data.login_disabled) 
         app.mount(mountPoint)
       } else {
-        console.log('NOT authenticated')
         // Redirect to login passing current location as the eventual destination
         let dest = encodeURIComponent(window.location.href)
         window.location.href = process.env.VUE_APP_BRAVO_API_URL + '/login?dest=' + dest
