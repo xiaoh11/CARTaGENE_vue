004ed358779db1a34d5332006de1d8251b9c6308
Colin Gross <grosscol@gmail.com>
2023-04-28 12:25:53 -0400

    Remove debug text

--- src/pages/variant/Variant.vue
+++ src/pages/variant/Variant.vue
@@ -48,7 +48,6 @@
 
         <div class="row">
           <div class="col-12 mt-3">
-            <pre>Reads Placeholder</pre>
             <Reads :variant="this.variant"/>
           </div>
         </div>
